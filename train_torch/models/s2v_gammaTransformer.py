08b641cabfc186f5b815e3995638f88fead2571d
KalickiD <danielkalicki@gmail.com>
2020-10-12 17:32:12 +0200

    model cleanup

--- train_torch/models/s2v_gammaTransformer.py
+++ train_torch/models/s2v_gammaTransformer.py
@@ -149,54 +149,18 @@ class TransformerEncoderLayer(nn.Module):
         self.linear1 = nn.Linear(d_model, dim_feedforward)
         self.activation = _get_activation_fn(activation)
         self.dropout = nn.Dropout(dropout)
-        # self.conv2 = nn.Conv1d(dim_feedforward, d_model, 3, padding=1)
-        # self.linear2 = DenseLayer(dim_feedforward, dim_feedforward, d_model, cat_dim=2)
-        # self.self_attn_ffn = MultiheadAttention(dim_feedforward, nhead, dropout=dropout, out_dim_mult=4.0)
         self.linear2 = nn.Linear(dim_feedforward, d_model)
 
-        # self.linear1_0 = nn.Linear(d_model, dim_feedforward)
-        # self.linear1_1 = nn.Linear(d_model, dim_feedforward)
-        # self.linear1_2 = nn.Linear(d_model, dim_feedforward)
-        # self.linear1_3 = nn.Linear(d_model, dim_feedforward)
-        # self.dropout_0 = nn.Dropout(0.1)
-        # self.dropout_1 = nn.Dropout(0.1)
-        # self.dropout_2 = nn.Dropout(0.1)
-        # self.dropout_3 = nn.Dropout(0.1)
-
-        # self.linear1_mha = nn.Linear(d_model//4, d_model)
-        # self.linear1_mha = DenseLayer(d_model, dim_feedforward, dim_feedforward, cat_dim=2)
-        # self.activation_mha = _get_activation_fn(activation)
-        # self.dropout_mha = nn.Dropout(dropout)
-        # self.linear2_mha = nn.Linear(d_model, d_model)
-
-        # self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
-        # self.self_attn = MultiheadAttention(d_model, nhead, dropout=dropout, out_dim_mult=0.5)
         self.self_attn = MultiheadAttention(d_model, nhead, dropout=dropout, out_dim_mult=1.0)
         self.linear_mha = nn.Linear(d_model, d_model)
-        # self.self_attn2 = MultiheadAttention(d_model, nhead, dropout=dropout, out_dim_mult=1.0)
         self.dropout1 = nn.Dropout(dropout)
         self.dropout2 = nn.Dropout(dropout)
-        # self.dropout3 = nn.Dropout(dropout)
 
         self.norm1 = nn.LayerNorm(d_model)
-        # self.norm1_ = nn.LayerNorm(d_model*2)
         self.norm2 = nn.LayerNorm(d_model)
-        # self.norm2_ = nn.LayerNorm(d_model*2)
 
         self.gate1 = nn.Linear(d_model*2, d_model)
-        # self.gate1 = MultiheadAttention(d_model*2, nhead, dropout=dropout, out_dim_mult=0.5)
-        # self.gate1_act = F.gelu
-        # self.gate1_l2 = nn.Linear(d_model, d_model)
-
-        # self.gate1_rnn = nn.GRU(d_model, d_model//2, bidirectional=True)
-        # self.gate2_rnn = nn.GRU(d_model, d_model//2, bidirectional=True)
-
         self.gate2 = nn.Linear(d_model*2, d_model)
-        # self.gate2 = MultiheadAttention(d_model*2, nhead, dropout=dropout, out_dim_mult=0.5)
-        # self.gate2_act = F.gelu
-        # self.gate2_l2 = nn.Linear(d_model, d_model)
-
-        # self.src_gate = nn.Linear(d_model, d_model)
 
     def __setstate__(self, state):
         if 'activation' not in state:
@@ -216,71 +180,26 @@ class TransformerEncoderLayer(nn.Module):
             see the docs in Transformer class.
         """
         src_ = self.norm1(src)
-        # src_ = src
         src2 = self.self_attn(src_, src_, src_, attn_mask=src_mask,
                               key_padding_mask=src_key_padding_mask)[0]
-        # src2 = self.self_attn2(src2, src2, src2, attn_mask=src_mask,
-        #                       key_padding_mask=src_key_padding_mask)[0]
-        # src2 = self.self_attn2(src2, src2, src2, attn_mask=src_mask,
-        #                       key_padding_mask=src_key_padding_mask)[0]
-        # src2 = torch.nn.functional.normalize(src2, dim=2)
         src2 = self.linear_mha(F.gelu(src2))
-        # g2 = torch.sigmoid(self.gate1_l2(self.gate1_act(self.gate1(torch.cat((src_, src2), dim=2)))))
-        # g2 = self.gate1_l2(self.gate1_act(self.gate1(torch.cat((src_, src2), dim=2))))
-        # g2 = torch.sigmoid(self.gate1(torch.cat((src_, src2), dim=2)))
         g2 = self.gate1(torch.cat((src_, src2), dim=2))
         g2_shape = g2.shape
-        # g2 = g2.reshape((g2_shape[0], g2_shape[1], 32, 32))
         g2 = g2.reshape((g2_shape[0], g2_shape[1], 128, 8))
-        # g2 = g2.reshape((g2_shape[0], g2_shape[1], 64, 8))
         g2 = torch.softmax(g2, dim=-1)
         g2 = g2.reshape((g2_shape[0], g2_shape[1], g2_shape[2]))
-        # g2 = torch.sigmoid(self.gate1(src2 + src_))
-        # src_cat = torch.cat((src_, src2), dim=2)
-        # g2 = torch.sigmoid(self.gate1_l2(self.gate1_act(self.gate1(src_cat,src_cat,src_cat, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)[0])))
-        # g2 = torch.tanh(torch.abs(self.gate1(self.gate1_rnn(src2)[0])))
-        # g2 = torch.tanh(torch.abs(src2))
-        # src2 = self.gate1_l2(self.gate1_act(self.gate1(torch.cat((src_, src2), dim=2))))
         src2 = src2 * g2
-        # src = src + self.dropout1(src2)*0.5
         src = src + self.dropout1(src2)
-        # src = self.dropout1(src)*0.95 + src2
-        # src = self.norm1(src)
 
         src_ = self.norm2(src)
-        # src_ = self.norm2(torch.cat((src, src2), dim=2))
-        # src_ = src
-        # src_ = (self.dropout_0(self.linear1_0(src_)) + self.dropout_1(self.linear1_1(src_)) + self.dropout_2(self.linear1_2(src_)) + self.dropout_3(self.linear1_3(src_))) / 4
-        # src2 = self.linear2(self.dropout(self.activation(src_)))
         src2 = self.linear2(self.dropout(self.activation(self.linear1(src_))))
-        # src2 = self.dropout(self.activation(self.linear1(src_)))
-        # src2 = self.conv2(src2.permute(0,2,1)).permute(0,2,1)
-        # src2 = self.conv1(src.permute(1,2,0)).permute(2,0,1)
-        # src2 = self.linear2(self.dropout(self.activation(src2)))
-        # g2 = torch.sigmoid(self.gate2(torch.cat((src, src2), dim=2)))
-        # g2 = torch.sigmoid(self.gate2_l2(self.gate2_act(self.gate2(torch.cat((src_, src2), dim=2)))))
-        # g2 = torch.sigmoid(self.gate2(torch.cat((src_, src2), dim=2)))
-        # g2 = torch.sigmoid(self.gate2(torch.cat((src_, src2), dim=2)))
         g2 = self.gate2(torch.cat((src_, src2), dim=2))
         g2_shape = g2.shape
-        # g2 = g2.reshape((g2_shape[0], g2_shape[1], 32, 32))
         g2 = g2.reshape((g2_shape[0], g2_shape[1], 128, 8))
-        # g2 = g2.reshape((g2_shape[0], g2_shape[1], 64, 8))
         g2 = torch.softmax(g2, dim=-1)
         g2 = g2.reshape((g2_shape[0], g2_shape[1], g2_shape[2]))
-        # g2 = torch.sigmoid(self.gate2(src2 + src_))
-        # src_cat = torch.cat((src_, src2), dim=2)
-        # g2 = torch.sigmoid(self.gate2_l2(self.gate2_act(self.gate2(src_cat,src_cat,src_cat, attn_mask=src_mask, key_padding_mask=src_key_padding_mask)[0])))
-        # g2 = torch.tanh(torch.abs(self.gate2(src2)))
-        # g2 = torch.tanh(torch.abs(self.gate2(self.gate2_rnn(src2)[0])))
-        # g2 = torch.tanh(torch.abs(src2))
-        # src2 = self.gate2(src_)
-        # g2 = self.gate2_l2(self.gate2_act(self.gate2(torch.cat((src_, src2), dim=2))))
         src2 = src2 * g2
-        # src = src + self.dropout2(src2)*0.5
         src = src + self.dropout2(src2)
-        # src = self.dropout2(src)*0.95 + src2
-        # src = self.norm2(src)
 
         return src
 
@@ -301,10 +220,6 @@ class SentenceEncoder(nn.Module):
         dim_feedforward = config['sentence_encoder']['transformer']['ffn_dim']
         tr_drop = config['sentence_encoder']['transformer']['residual_dropout']
         
-        # self.input_fc = nn.Linear(word_edim, int(word_edim//2))
-
-        # word_edim = int(word_edim*1.5)
-
         self.norm2 = nn.LayerNorm(word_edim)
 
         encoder_layer = TransformerEncoderLayer(d_model=word_edim, nhead=num_head,
@@ -317,8 +232,6 @@ class SentenceEncoder(nn.Module):
         mha_drop = config['sentence_encoder']['pooling']['mha']['attention_dropout']
         out_dim_mult = mha_dim/word_edim
 
-        # self.mha_pool_v = MultiheadAttention(word_edim, mha_nhead, dropout=mha_drop, out_dim_mult=1.0)
-        # self.fc_logvar = nn.Linear(word_edim, mha_dim)
         self.mha_pool = MultiheadAttention(word_edim, mha_nhead, dropout=mha_drop, out_dim_mult=out_dim_mult)
 
         # nli classifier
@@ -342,10 +255,14 @@ class SentenceEncoder(nn.Module):
         self.qq_act1 = _get_activation_fn(class_hact)
         self.qq_fc2 = nn.Linear(class_hdim, 2)
 
-        # self.fc_words = nn.Linear(mha_dim, 10612)
-        # self.fc2_words = nn.Linear(1, 2)
-        # self.act1_ = _get_activation_fn(class_hact)
-        # self.fc2_ = nn.Linear(class_hdim, num_classes)
+        self.blimp_fc1 = nn.Linear(mha_dim, 128)
+        self.blimp_act1 = _get_activation_fn(class_hact)
+        self.blimp_fc2 = nn.Linear(128, 2)
+
+        self.paws_fc1_dr = nn.Dropout(class_dropout)
+        self.paws_fc1 = nn.Linear(4*mha_dim, class_hdim)
+        self.paws_act1 = _get_activation_fn(class_hact)
+        self.paws_fc2 = nn.Linear(class_hdim, 2)
 
     def _emb_sent(self, sent, sent_mask):
         sent = self.in_dr(sent)
@@ -355,23 +272,15 @@ class SentenceEncoder(nn.Module):
         sent = self.gtr(sent, src_key_padding_mask=sent_mask)
         sent_ = self.norm2(sent)
 
-        # sent_v, _ = self.mha_pool_v(sent, sent, sent, key_padding_mask=sent_mask)
         sent, _ = self.mha_pool(sent_, sent_, sent_, key_padding_mask=sent_mask)
-        # sent_logvar = self.fc_logvar(sent_)
-
-        # std = torch.exp(0.5*sent_logvar) # standard deviation
-        # eps = torch.randn_like(std)
-        # sample = sent_mu + (eps * std)
-        # sent = sample
 
         sent = sent.permute((1, 0, 2))
 
         sent_mask_exp = torch.cat([sent_mask.unsqueeze(2)]*sent.shape[2], dim=2).type(torch.cuda.FloatTensor)
         # s2v, _ = torch.max(sent + sent_mask_exp*-1e3, axis=1)
-
         s2v = torch.sum(sent*(1-sent_mask_exp), axis=1) / torch.sum((1-sent_mask_exp), axis=1)
 
-        s2v = s2v + torch.mean(s2v)*torch.randn_like(s2v)*0.1
+        # s2v = s2v + torch.mean(s2v)*torch.randn_like(s2v)*0.1
 
         return s2v
 
@@ -380,29 +289,40 @@ class SentenceEncoder(nn.Module):
         s2v_sent2 = self._emb_sent(sent2, sent2_mask)
 
         x_class_in = torch.cat((s2v_sent1, s2v_sent2, torch.abs(s2v_sent1-s2v_sent2), s2v_sent1*s2v_sent2), dim=1)
-        # x_class_in = torch.cat((s2v_sent1, s2v_sent2, torch.abs(s2v_sent1-s2v_sent2)), dim=1)
         x_class_in = self.fc1_dr(x_class_in)
         x_class = self.fc1(x_class_in)
         x_class = self.act1(x_class)
-        x_class = self.fc2(x_class)
+        nli_class = self.fc2(x_class)
 
         zs_class_in = torch.cat((s2v_sent1, s2v_sent2, torch.abs(s2v_sent1-s2v_sent2), s2v_sent1*s2v_sent2), dim=1)
-        # zs_class_in = torch.cat((s2v_sent1, s2v_sent2, torch.abs(s2v_sent1-s2v_sent2)), dim=1)
         zs_class_in = self.zs_fc1_dr(zs_class_in)
         zs_class = self.zs_fc1(zs_class_in)
         zs_class = self.zs_act1(zs_class)
         zs_class = self.zs_fc2(zs_class)
 
         qq_class_in = torch.cat((s2v_sent1, s2v_sent2, torch.abs(s2v_sent1-s2v_sent2), s2v_sent1*s2v_sent2), dim=1)
-        # qq_class_in = torch.cat((s2v_sent1, s2v_sent2, torch.abs(s2v_sent1-s2v_sent2)), dim=1)
         qq_class_in = self.qq_fc1_dr(qq_class_in)
         qq_class = self.qq_fc1(qq_class_in)
         qq_class = self.qq_act1(qq_class)
         qq_class = self.qq_fc2(qq_class)
 
-        # words_1 = self.fc_words(s2v_sent2)
-        # words_1 = F.relu(words_1)
-        # words_1 = torch.unsqueeze(words_1, dim=-1)
-        # words_pred = self.fc2_words(words_1)
+        blimp_class_in = s2v_sent1
+        blimp_class = self.blimp_fc1(blimp_class_in)
+        blimp_class = self.blimp_act1(blimp_class)
+        blimp_class = self.blimp_fc2(blimp_class)
 
-        return s2v_sent1, s2v_sent2, x_class, zs_class, qq_class #, words_pred #, x_class_
+        paws_class_in = torch.cat((s2v_sent1, s2v_sent2, torch.abs(s2v_sent1-s2v_sent2), s2v_sent1*s2v_sent2), dim=1)
+        paws_class_in = self.paws_fc1_dr(paws_class_in)
+        paws_class = self.paws_fc1(paws_class_in)
+        paws_class = self.paws_act1(paws_class)
+        paws_class = self.paws_fc2(paws_class)
+
+        return {
+            's2v_sent1': s2v_sent1,
+            's2v_sent2': s2v_sent2,
+            'nli_pred': nli_class,
+            'zs_pred': zs_class,
+            'qq_pred': qq_class,
+            'blimp_pred': blimp_class,
+            'paws_pred': paws_class
+        }
